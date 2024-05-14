6271e072faff967083c0cab167d118afb799fe42
GabrielRudger0 <gabrielrudger2003@gmail.com>
2024-05-14 13:05:59 -0300

    [Novo] - Versão base do sistema [Descrição] - Este commit implementa a versão base do sistema.

---
+++ src/main/java/com/senai/GetEPI/GetEpiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.GetEPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetEpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetEpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/GetEPI/GetEpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.GetEPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetEpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
