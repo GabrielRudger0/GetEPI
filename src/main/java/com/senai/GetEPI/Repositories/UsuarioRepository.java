e928b65593a303a935a00558c9af6a6b5bc8ce0f
GabrielRudger0 <gabrielrudger2003@gmail.com>
2024-05-18 14:20:53 -0300

    [Novo] Manter usuário v1.1 [Descrição] Implementado os atributos no UsuarioModel e UsuarioDTO. Implementado o repository e versão base da lista de usuario

--- src/main/java/com/senai/GetEPI/Repositories/UsuarioRepository.java
+++ src/main/java/com/senai/GetEPI/Repositories/UsuarioRepository.java
@@ -1,4 +1,7 @@
 package com.senai.GetEPI.Repositories;
 
-public class UsuarioRepository {
+import com.senai.GetEPI.Models.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
 }
