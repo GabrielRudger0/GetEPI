d4862513b3d0ddff83f33c644d72b9b34c6bf6ca
GabrielRudger0 <gabrielrudger2003@gmail.com>
2024-05-18 16:18:51 -0300

    [Novo] Manter usuário v1.2 [Descrição] Implementado HTML da tela de listausuario.html e implementado HTML de template para futuras páginas

--- src/main/java/com/senai/GetEPI/Controllers/Usuario/ListarUsuarioController.java
+++ src/main/java/com/senai/GetEPI/Controllers/Usuario/ListarUsuarioController.java
@@ -2,12 +2,14 @@ package com.senai.GetEPI.Controllers.Usuario;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/listausuario")
 public class ListarUsuarioController {
 
+    @GetMapping
     public String exibeListaUsuario(Model model) {
         return "listausuario";
     }
