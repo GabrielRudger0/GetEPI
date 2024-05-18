07c574bd2ba38557524e1557939d3cd70f016c90
lucaneta <lucas99ney@gmail.com>
2024-05-18 17:11:51 -0300

    Commit colaborador/login

---
+++ src/main/java/com/senai/GetEPI/Controllers/Colaborador/AtualizarColaboradorController.java
@@ -0,0 +1,10 @@
+package com.senai.GetEPI.Controllers.Colaborador;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class AtualizarColaboradorController {
+}
---
+++ src/main/java/com/senai/GetEPI/Controllers/Colaborador/CadastrarColaboradorController.java
@@ -0,0 +1,9 @@
+package com.senai.GetEPI.Controllers.Colaborador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class CadastrarColaboradorController {
+}
