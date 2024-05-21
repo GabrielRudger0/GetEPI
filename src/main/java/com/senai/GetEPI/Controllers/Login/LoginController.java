a1f0a83703ac0a3a95a81edb18d1ffc1603a0748
gabriel_rudger <gabrielrudger2003@gmail.com>
2024-05-20 21:18:46 -0300

    [Novo] - Login v1.5 [Descrição] - Autentiação do login 1.0

--- src/main/java/com/senai/GetEPI/Controllers/Login/LoginController.java
+++ src/main/java/com/senai/GetEPI/Controllers/Login/LoginController.java
@@ -1,21 +1,38 @@
 package com.senai.GetEPI.Controllers.Login;
 
 import com.senai.GetEPI.DTOs.LoginDTO;
+import com.senai.GetEPI.Services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/login")
 public class LoginController {
 
+    @Autowired
+    UsuarioService usuarioService;
 
-    @GetMapping("/login")
+    @GetMapping
     public String mostrarLogin(Model model) {
 
         LoginDTO login = new LoginDTO();
         model.addAttribute("loginDto", login);
+        return "login";
+    }
+
+    @PostMapping
+    public String efetuarLogin(@ModelAttribute("loginDto") LoginDTO login, Model model) {
+
+        if (usuarioService.loginValido(login)) {
+
+            return "redirect:/listausuario";
+        }
+        model.addAttribute("erro", true);
         return "login";
     }
 
--- src/main/java/com/senai/GetEPI/Repositories/UsuarioRepository.java
+++ src/main/java/com/senai/GetEPI/Repositories/UsuarioRepository.java
@@ -4,9 +4,12 @@ import com.senai.GetEPI.Models.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
     public boolean existsByEmail(String email);
 
     public List<UsuarioModel> findByNomeContaining(String nome);
+
+    public Optional<UsuarioModel> findByEmail(String email);
 }
