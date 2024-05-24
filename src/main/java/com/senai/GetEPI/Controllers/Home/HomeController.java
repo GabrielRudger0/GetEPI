b795e4ea2c99babf6d0e0e77a1e49ded41820a75
GabrielRudger0 <gabrielrudger2003@gmail.com>
2024-05-23 22:08:37 -0300

    Home 1.3 SideBar 1.7

--- src/main/java/com/senai/GetEPI/Controllers/Home/HomeController.java
+++ src/main/java/com/senai/GetEPI/Controllers/Home/HomeController.java
@@ -11,7 +11,10 @@ public class HomeController {
 
     @GetMapping()
     public String exibirHome(Model model) {
-        model.addAttribute("valor1teste", 250);
+        model.addAttribute("worldwide_epi_1", "EPI1");
+        model.addAttribute("worldwide_epi_2", "EPI2");
+        model.addAttribute("worldwide_epi_3", "EPI3");
+
         return "home";
     }
 
