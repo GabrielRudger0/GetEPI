90b0f9438e7ac54a8e82af4cfeb10bbcdc41c3af
lucaneta <lucas99ney@gmail.com>
2024-05-25 00:01:58 -0300

    commit funcões do colaborador pt4(fim, finalmente)

--- src/main/java/com/senai/GetEPI/Controllers/Funcao/AtualizarFuncaoController.java
+++ src/main/java/com/senai/GetEPI/Controllers/Funcao/AtualizarFuncaoController.java
@@ -1,4 +1,38 @@
 package com.senai.GetEPI.Controllers.Funcao;
 
+import com.senai.GetEPI.DTOs.ColaboradorDto;
+import com.senai.GetEPI.DTOs.FuncaoDto;
+import com.senai.GetEPI.Services.FuncaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/atualizarfuncao")
 public class AtualizarFuncaoController {
+
+    @Autowired
+    FuncaoService funcaoService;
+
+    @GetMapping("/{id}")
+    public String exibeAtualizaFuncao(Model model, @PathVariable Long id) {
+        FuncaoDto funcao = funcaoService.buscaFuncaoDTO(id);
+        model.addAttribute("funcaoDTO", funcao);
+        return "atualizarfuncao";
+    }
+
+    @PostMapping()
+    public String botaoSalvar(@ModelAttribute("funcaoDTO") FuncaoDto funcao, Model model) {
+        String mensagemErro = funcaoService.atualizarFuncao(funcao);
+
+        if (!mensagemErro.isEmpty()) {
+            model.addAttribute("erro", true);
+            model.addAttribute("mensagemErro", mensagemErro);
+            return "atualizarfuncao";
+        }
+        return "redirect:/listafuncao";
+
+    }
+
 }
--- src/main/java/com/senai/GetEPI/Repositories/FuncaoRepository.java
+++ src/main/java/com/senai/GetEPI/Repositories/FuncaoRepository.java
@@ -4,8 +4,10 @@ import com.senai.GetEPI.Models.FuncaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FuncaoRepository extends JpaRepository<FuncaoModel, Long> {
 
-
+    Optional<FuncaoModel> findByFuncao(String funcao);
 }
