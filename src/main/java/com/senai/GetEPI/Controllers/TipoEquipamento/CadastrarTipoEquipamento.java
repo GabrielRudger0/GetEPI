1f22f56f70bd054771552dd9996f8e5dffa5a815
GabrielRudger0 <gabrielrudger2003@gmail.com>
2024-06-08 22:05:43 -0300

    RF12 - Implementar tratamento de erro Try-Catch Implementado no fluxo de tipo de equipamento

--- src/main/java/com/senai/GetEPI/Controllers/TipoEquipamento/CadastrarTipoEquipamento.java
+++ src/main/java/com/senai/GetEPI/Controllers/TipoEquipamento/CadastrarTipoEquipamento.java
@@ -2,6 +2,7 @@ package com.senai.GetEPI.Controllers.TipoEquipamento;
 
 import com.senai.GetEPI.DTOs.TipoEquipamentoDTO;
 import com.senai.GetEPI.Services.TipoEquipamentoService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,22 +19,35 @@ public class CadastrarTipoEquipamento {
     TipoEquipamentoService tipoEquipamentoService;
 
     @GetMapping()
-    public String exibeCadastrarTipoEquipamento(Model model) {
-
-        model.addAttribute("tipoEquipamentoDTO", new TipoEquipamentoDTO());
-        return "cadastrartipoequipamento";
+    public String exibeCadastrarTipoEquipamento(Model model, HttpServletRequest request) {
+        try {
+            model.addAttribute("tipoEquipamentoDTO", new TipoEquipamentoDTO());
+            return "cadastrartipoequipamento";
+        } catch (Exception e) {
+            request.getSession().setAttribute("retornaErro", e);
+            request.getSession().setAttribute("stacktrace", e);
+            return "redirect:/listatipoequipamento";
+        }
     }
 
     @PostMapping()
-    public String cadastrarTipoEquipamento(@ModelAttribute("tipoEquipamentoDTO") TipoEquipamentoDTO tipoEquipamento, Model model) {
-        String mensagemErro = tipoEquipamentoService.cadastraTipoEquipamento(tipoEquipamento);
+    public String cadastrarTipoEquipamento(@ModelAttribute("tipoEquipamentoDTO") TipoEquipamentoDTO tipoEquipamento, Model model, HttpServletRequest request) {
+        try {
+            String mensagemErro = tipoEquipamentoService.cadastraTipoEquipamento(tipoEquipamento);
 
-        if (!mensagemErro.isEmpty()) {
-            model.addAttribute("erro", true);
-            model.addAttribute("mensagemErro", mensagemErro);
-            return "cadastrartipoequipamento";
+            if (!mensagemErro.isEmpty()) {
+                model.addAttribute("erro", true);
+                model.addAttribute("mensagemErro", mensagemErro);
+                return "cadastrartipoequipamento";
+            }
+
+            return "redirect:/listatipoequipamento";
+
+        } catch (Exception e) {
+            request.getSession().setAttribute("retornaErro", e);
+            request.getSession().setAttribute("stacktrace", e);
+            return "redirect:/listatipoequipamento";
         }
 
-        return "redirect:/listatipoequipamento";
     }
 }
