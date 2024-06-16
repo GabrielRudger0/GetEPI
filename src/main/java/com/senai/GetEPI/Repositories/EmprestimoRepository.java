93260ebf74b6b191aac19220ee8f95e8bef80c72
GabrielRudger0 <gabrielrudger2003@gmail.com>
2024-06-16 13:24:37 -0300

    Refatoração da home

--- src/main/java/com/senai/GetEPI/Repositories/EmprestimoRepository.java
+++ src/main/java/com/senai/GetEPI/Repositories/EmprestimoRepository.java
@@ -14,35 +14,35 @@ import java.util.Optional;
 @Repository
 public interface EmprestimoRepository extends JpaRepository<EmprestimoModel, Long> {
 
-    @Query(value = "SELECT * FROM emprestimo WHERE emprestimo_devolucao IS NULL", nativeQuery = true)
+    @Query(value = "SELECT * FROM emprestimo WHERE emprestimo_devolucao IS NULL AND registro_interno = 0", nativeQuery = true)
     public List<EmprestimoModel> findAllWhereEmprestimoDevolucaoIsNull();
 
     @Override
     @Query(value = "SELECT * FROM emprestimo WHERE registro_interno = 0", nativeQuery = true)
     public List<EmprestimoModel> findAll();
 
-    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE epi_id_epi = ?", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE epi_id_epi = ? AND registro_interno = 0", nativeQuery = true)
     public Integer quantidadeEmprestimoEPI(Long id);
 
-    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE epi_id_epi = ? AND emprestimo_emissao_data > ? AND emprestimo_emissao_data < ?", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE epi_id_epi = ? AND emprestimo_emissao_data > ? AND emprestimo_emissao_data < ? AND registro_interno = 0", nativeQuery = true)
     public Integer quantidadeEmprestimoEPIPorMes(Long id, String dataInicio, String dataFim);
 
-    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE emprestimo_emissao_data > ? AND emprestimo_emissao_data < ?", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE emprestimo_emissao_data > ? AND emprestimo_emissao_data < ? AND registro_interno = 0", nativeQuery = true)
     public Integer quantidadeEmprestimoPorDia(String dataInicio, String dataFim);
 
-    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE emprestimo_devolucao > ? AND emprestimo_devolucao < ?", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM emprestimo WHERE emprestimo_devolucao > ? AND emprestimo_devolucao < ? AND registro_interno = 0", nativeQuery = true)
     public Integer quantidadeDevolucaoPorDia(String dataInicio, String dataFim);
 
     @Query(value = "SELECT * FROM emprestimo WHERE colaborador_id_colaborador = ? AND epi_id_epi = ? AND emprestimo_devolucao IS NULL LIMIT 1", nativeQuery = true)
     public Optional<EmprestimoModel> existeEmprestimoVigente(Long colaboradorId, Long epiId);
 
-    @Query(value = "SELECT * FROM emprestimo WHERE colaborador_id_colaborador = ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM emprestimo WHERE colaborador_id_colaborador = ? AND registro_interno = 0", nativeQuery = true)
     public List<EmprestimoModel> findAllByColaboradorId(Long colaboradorId);
 
-    @Query(value = "SELECT * FROM emprestimo WHERE colaborador_id_colaborador = ? AND emprestimo_devolucao IS NULL", nativeQuery = true)
+    @Query(value = "SELECT * FROM emprestimo WHERE colaborador_id_colaborador = ? AND registro_interno = 0 AND emprestimo_devolucao IS NULL", nativeQuery = true)
     public List<EmprestimoModel> findAllDevolucoesByColaboradorId(Long colaboradorId);
 
-    @Query(value = "SELECT * FROM emprestimo WHERE epi_id_epi = ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM emprestimo WHERE epi_id_epi = ? AND registro_interno = 0", nativeQuery = true)
     public List<EmprestimoModel> findAllByEpiId(Long epiId);
 
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,75 +1,7 @@
 (function ($) {
     "use strict";
 
-    // epis mais utilizados
-    var worldwide_epi_1 = document.getElementById('worldwide_epi_1').innerText;
-    var worldwide_epi_2 = document.getElementById('worldwide_epi_2').innerText;
-    var worldwide_epi_3 = document.getElementById('worldwide_epi_3').innerText;
-
-    // sete meses anterires
-    var worldwide_mes_1 = document.getElementById('worldwide_mes_1').innerText;
-    var worldwide_mes_2 = document.getElementById('worldwide_mes_2').innerText;
-    var worldwide_mes_3 = document.getElementById('worldwide_mes_3').innerText;
-    var worldwide_mes_4 = document.getElementById('worldwide_mes_4').innerText;
-    var worldwide_mes_5 = document.getElementById('worldwide_mes_5').innerText;
-    var worldwide_mes_6 = document.getElementById('worldwide_mes_6').innerText;
-    var worldwide_mes_7 = document.getElementById('worldwide_mes_7').innerText;
-
-    // Quantidade de emprestimos do epi1 nos meses estipulados
-    var worldwide_mes1_epi1 = document.getElementById('worldwide_mes1_epi1').innerText;
-    var worldwide_mes2_epi1 = document.getElementById('worldwide_mes2_epi1').innerText;
-    var worldwide_mes3_epi1 = document.getElementById('worldwide_mes3_epi1').innerText;
-    var worldwide_mes4_epi1 = document.getElementById('worldwide_mes4_epi1').innerText;
-    var worldwide_mes5_epi1 = document.getElementById('worldwide_mes5_epi1').innerText;
-    var worldwide_mes6_epi1 = document.getElementById('worldwide_mes6_epi1').innerText;
-    var worldwide_mes7_epi1 = document.getElementById('worldwide_mes7_epi1').innerText;
-
-    // Quantidade de emprestimos do epi2 nos meses estipulados
-    var worldwide_mes1_epi2 = document.getElementById('worldwide_mes1_epi2').innerText;
-    var worldwide_mes2_epi2 = document.getElementById('worldwide_mes2_epi2').innerText;
-    var worldwide_mes3_epi2 = document.getElementById('worldwide_mes3_epi2').innerText;
-    var worldwide_mes4_epi2 = document.getElementById('worldwide_mes4_epi2').innerText;
-    var worldwide_mes5_epi2 = document.getElementById('worldwide_mes5_epi2').innerText;
-    var worldwide_mes6_epi2 = document.getElementById('worldwide_mes6_epi2').innerText;
-    var worldwide_mes7_epi2 = document.getElementById('worldwide_mes7_epi2').innerText;
-
-    // Quantidade de emprestimos do epi3 nos meses estipulados
-    var worldwide_mes1_epi3 = document.getElementById('worldwide_mes1_epi3').innerText;
-    var worldwide_mes2_epi3 = document.getElementById('worldwide_mes2_epi3').innerText;
-    var worldwide_mes3_epi3 = document.getElementById('worldwide_mes3_epi3').innerText;
-    var worldwide_mes4_epi3 = document.getElementById('worldwide_mes4_epi3').innerText;
-    var worldwide_mes5_epi3 = document.getElementById('worldwide_mes5_epi3').innerText;
-    var worldwide_mes6_epi3 = document.getElementById('worldwide_mes6_epi3').innerText;
-    var worldwide_mes7_epi3 = document.getElementById('worldwide_mes7_epi3').innerText;
-
-    // ------ grafico 2 ----------- //
-    var grafico2_diaSemana1 = document.getElementById('grafico2_diaSemana1').innerText;
-    var grafico2_diaSemana2 = document.getElementById('grafico2_diaSemana2').innerText;
-    var grafico2_diaSemana3 = document.getElementById('grafico2_diaSemana3').innerText;
-    var grafico2_diaSemana4 = document.getElementById('grafico2_diaSemana4').innerText;
-    var grafico2_diaSemana5 = document.getElementById('grafico2_diaSemana5').innerText;
-    var grafico2_diaSemana6 = document.getElementById('grafico2_diaSemana6').innerText;
-    var grafico2_diaSemana7 = document.getElementById('grafico2_diaSemana7').innerText;
-
-    var grafico2_diaSemana1_devolucoes = document.getElementById('grafico2_diaSemana1_devolucoes').innerText;
-    var grafico2_diaSemana2_devolucoes = document.getElementById('grafico2_diaSemana2_devolucoes').innerText;
-    var grafico2_diaSemana3_devolucoes = document.getElementById('grafico2_diaSemana3_devolucoes').innerText;
-    var grafico2_diaSemana4_devolucoes = document.getElementById('grafico2_diaSemana4_devolucoes').innerText;
-    var grafico2_diaSemana5_devolucoes = document.getElementById('grafico2_diaSemana5_devolucoes').innerText;
-    var grafico2_diaSemana6_devolucoes = document.getElementById('grafico2_diaSemana6_devolucoes').innerText;
-    var grafico2_diaSemana7_devolucoes = document.getElementById('grafico2_diaSemana7_devolucoes').innerText;
-
-    var grafico2_diaSemana1_emprestimo = document.getElementById('grafico2_diaSemana1_emprestimo').innerText;
-    var grafico2_diaSemana2_emprestimo = document.getElementById('grafico2_diaSemana2_emprestimo').innerText;
-    var grafico2_diaSemana3_emprestimo = document.getElementById('grafico2_diaSemana3_emprestimo').innerText;
-    var grafico2_diaSemana4_emprestimo = document.getElementById('grafico2_diaSemana4_emprestimo').innerText;
-    var grafico2_diaSemana5_emprestimo = document.getElementById('grafico2_diaSemana5_emprestimo').innerText;
-    var grafico2_diaSemana6_emprestimo = document.getElementById('grafico2_diaSemana6_emprestimo').innerText;
-    var grafico2_diaSemana7_emprestimo = document.getElementById('grafico2_diaSemana7_emprestimo').innerText;
-
-
-
-    // Spinner
+// Spinner
     var spinner = function () {
         setTimeout(function () {
             if ($('#spinner').length > 0) {
@@ -115,59 +47,136 @@
     Chart.defaults.color = "#6C7293";
     Chart.defaults.borderColor = "#000000";
 
-
-    // Worldwide Sales Chart
-    var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
-    var myChart1 = new Chart(ctx1, {
-        type: "bar",
-        data: {
-            labels: [worldwide_mes_1, worldwide_mes_2, worldwide_mes_3, worldwide_mes_4, worldwide_mes_5, worldwide_mes_6, worldwide_mes_7],
-            datasets: [{
-                    label: worldwide_epi_1,
-                    data: [worldwide_mes1_epi1, worldwide_mes2_epi1, worldwide_mes3_epi1, worldwide_mes4_epi1, worldwide_mes5_epi1, worldwide_mes6_epi1, worldwide_mes7_epi1],
-                    backgroundColor: "rgb(255, 213, 25, .7)"
-                },
-                {
-                    label: worldwide_epi_2,
-                    data: [worldwide_mes1_epi2, worldwide_mes2_epi2, worldwide_mes3_epi2, worldwide_mes4_epi2, worldwide_mes5_epi2, worldwide_mes6_epi2, worldwide_mes7_epi2],
-                    backgroundColor: "rgb(255, 213, 25, .5)"
-                },
-                {
-                    label: worldwide_epi_3,
-                    data: [worldwide_mes1_epi3, worldwide_mes2_epi3, worldwide_mes3_epi3, worldwide_mes4_epi3, worldwide_mes5_epi3, worldwide_mes6_epi3, worldwide_mes7_epi3],
-                    backgroundColor: "rgb(255, 213, 25, .3)"
-                }
-            ]
-            },
-        options: {
-            responsive: true
+    //CARREGAMENTO DO GRAFICO DE RESULTADOS DO MES (GRAFICO 1)
+    fetch(`/home/carregargraficoresultadosmes`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return response.text()
+                .then(data => {
+                    Swal.fire({
+                        title: "Erro ao carregar o gráfico!",
+                        text: data,
+                        confirmButtonColor: "#0d6efd",
+                        iconHtml: '<i class="fas fa-exclamation-circle" style="color: #dc3545; font-size: 5rem;"></i>'
+                    });
+                    throw new Error('Erro na requisição: ' + response.status);
+                });
         }
+    })
+    .then(data => {
+        var mes = data.meses;
+        var graficoEpis = data.graficoEPIs;
+        var graficoResultadosEPI1 = data.resultadosMesEPI1;
+        var graficoResultadosEPI2 = data.resultadosMesEPI2;
+        var graficoResultadosEPI3 = data.resultadosMesEPI3;
+
+        // CONFIGURAÇÃO DO GRAFICO 1
+        var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
+        var myChart1 = new Chart(ctx1, {
+            type: "bar",
+            data: {
+                labels: [mes[0], mes[1], mes[2], mes[3], mes[4], mes[5], mes[6]],
+                datasets: [{
+                        label: graficoEpis[0],
+                        data: [graficoResultadosEPI1[0], graficoResultadosEPI1[1], graficoResultadosEPI1[2], graficoResultadosEPI1[3], graficoResultadosEPI1[4], graficoResultadosEPI1[5], graficoResultadosEPI1[6]],
+                        backgroundColor: "rgb(255, 213, 25, .7)"
+                    },
+                    {
+                        label: graficoEpis[1],
+                        data: [graficoResultadosEPI2[0], graficoResultadosEPI2[1], graficoResultadosEPI2[2], graficoResultadosEPI2[3], graficoResultadosEPI2[4], graficoResultadosEPI2[5], graficoResultadosEPI2[6]],
+                        backgroundColor: "rgb(255, 213, 25, .5)"
+                    },
+                    {
+                        label: graficoEpis[2],
+                        data: [graficoResultadosEPI3[0], graficoResultadosEPI3[1], graficoResultadosEPI3[2], graficoResultadosEPI3[3], graficoResultadosEPI3[4], graficoResultadosEPI3[5], graficoResultadosEPI3[6]],
+                        backgroundColor: "rgb(255, 213, 25, .3)"
+                    }
+                ]
+                },
+            options: {
+                responsive: true
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Erro na requisição fetch:', error);
+        Swal.fire({
+            title: "Erro ao carregar o gráfico!",
+            text: "Ocorreu um erro na requisição.",
+            confirmButtonColor: "#0d6efd",
+            iconHtml: '<i class="fas fa-exclamation-circle" style="color: #dc3545; font-size: 5rem;"></i>'
+        });
     });
 
 
-    // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
-    var myChart2 = new Chart(ctx2, {
-        type: "line",
-        data: {
-            labels: [grafico2_diaSemana1, grafico2_diaSemana2, grafico2_diaSemana3, grafico2_diaSemana4, grafico2_diaSemana5, grafico2_diaSemana6, grafico2_diaSemana7],
-            datasets: [{
-                    label: "Devoluções",
-                    data: [grafico2_diaSemana1_devolucoes, grafico2_diaSemana2_devolucoes, grafico2_diaSemana3_devolucoes, grafico2_diaSemana4_devolucoes, grafico2_diaSemana5_devolucoes, grafico2_diaSemana6_devolucoes, grafico2_diaSemana7_devolucoes],
-                    backgroundColor: "rgb(255, 213, 25, .5)",
-                    fill: true
-                },
-                {
-                    label: "Empréstimos",
-                    data: [grafico2_diaSemana1_emprestimo, grafico2_diaSemana2_emprestimo, grafico2_diaSemana3_emprestimo, grafico2_diaSemana4_emprestimo, grafico2_diaSemana5_emprestimo, grafico2_diaSemana6_emprestimo, grafico2_diaSemana7_emprestimo],
-                    backgroundColor: "rgb(253, 126, 20, .5)",
-                    fill: true
-                }
-            ]
-            },
-        options: {
-            responsive: true
+    //CARREGAR GRAFICO MOVIMENTAÇÕES DA SEMANA
+    fetch(`/home/carregargraficomovimentacoessemana`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return response.text()
+                .then(data => {
+                    Swal.fire({
+                        title: "Erro ao carregar o gráfico!",
+                        text: data,
+                        confirmButtonColor: "#0d6efd",
+                        iconHtml: '<i class="fas fa-exclamation-circle" style="color: #dc3545; font-size: 5rem;"></i>'
+                    });
+                    throw new Error('Erro na requisição: ' + response.status);
+                });
         }
+    })
+    .then(data => {
+        var diasSemana = data.diasSemana;
+        var resultadoSemanaDevolucao = data.resultadoSemanaDevolucao;
+        var resultadoSemanaEmprestimo = data.resultadoSemanaEmprestimo;
+
+        // CONFIGURAÇÃO DO GRAFICO 2
+        var ctx2 = $("#salse-revenue").get(0).getContext("2d");
+        var myChart2 = new Chart(ctx2, {
+            type: "line",
+            data: {
+                labels: [diasSemana[0], diasSemana[1], diasSemana[2], diasSemana[3], diasSemana[4], diasSemana[5], diasSemana[6]],
+                datasets: [{
+                        label: "Devoluções",
+                        data: [resultadoSemanaDevolucao[0], resultadoSemanaDevolucao[1], resultadoSemanaDevolucao[2], resultadoSemanaDevolucao[3], resultadoSemanaDevolucao[4], resultadoSemanaDevolucao[5], resultadoSemanaDevolucao[6]],
+                        backgroundColor: "rgb(255, 213, 25, .5)",
+                        fill: true
+                    },
+                    {
+                        label: "Empréstimos",
+                        data: [resultadoSemanaEmprestimo[0], resultadoSemanaEmprestimo[1], resultadoSemanaEmprestimo[2], resultadoSemanaEmprestimo[3], resultadoSemanaEmprestimo[4], resultadoSemanaEmprestimo[5], resultadoSemanaEmprestimo[6]],
+                        backgroundColor: "rgb(253, 126, 20, .5)",
+                        fill: true
+                    }
+                ]
+                },
+            options: {
+                responsive: true
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Erro na requisição fetch:', error);
+        Swal.fire({
+            title: "Erro ao carregar o gráfico!",
+            text: error,
+            confirmButtonColor: "#0d6efd",
+            iconHtml: '<i class="fas fa-exclamation-circle" style="color: #dc3545; font-size: 5rem;"></i>'
+        });
     });
 
 })(jQuery);
